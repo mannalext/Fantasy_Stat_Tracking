18026df442d5fe1ab8f4b2e75c9751f4e8301161
southheat92 <southheat92@gmail.com>
2015-09-11 17:31:09 -0400

    first commit with progress
    starting TrackerRobot.java

---
+++ Stat_Tracker/src/atm/stat/TrackerRobot/TrackerRobot.java
@@ -0,0 +1,49 @@
+package atm.stat.TrackerRobot;
+
+import java.io.File;
+import jxl.*; 
+
+public class TrackerRobot {
+	
+	final static int teamDataSize = 8;
+
+	public static void main(String[] args) {
+		try 
+		{
+			 Workbook workbook = Workbook.getWorkbook(new File("/Users/alexmann/Developer/Tracker_Robot/inputFile_old.xls")); 	
+			 
+			 //getting number of teams
+		     Sheet sheet = workbook.getSheet(0); 	     
+		     Cell a2 = sheet.getCell(0,1); 
+		     String teamCount = a2.getContents();
+		     
+		     System.out.println(teamCount);
+		     
+		     extractData(sheet, teamCount);
+		     
+		} catch (Exception e)
+		{
+			System.out.println(e.getMessage());
+		}
+	}
+
+	//todo - finish extracting data
+	private static void extractData(Sheet sheet, String teamCount) {
+		int teamCountInt = Integer.parseInt(teamCount);
+		int dataSize = teamCountInt * teamDataSize;
+		
+		String[] data = new String[dataSize];
+		int pos = 0;
+		
+		for (int x = 0; x < teamCountInt; x++)
+		{
+			int col = 1;
+			int row = 1;
+			for (int y = 0; x < teamDataSize; x++)
+			{
+				Cell teamName = sheet.getCell(col, row);
+			}
+		}
+	}
+
+}
