5485f955750f3efdf78eaede98e4b9be08fa544d
southheat92 <southheat92@gmail.com>
2015-09-12 11:58:03 -0400

    more refinements to extractData, started massageData
    extractData is now numWeek and numTeam agnostic. additionally,
    massageData will go and pull a single team's lifetime numbers. also
    started laying the groundwork for seasonal / lifetime statistics

--- README.md
+++ README.md
@@ -7,8 +7,58 @@ Finish the three modules
 1) Data collection
 2) Data massaging
 3) Data delivery
-4) review
+4) ctrl+f "todo"
+5) review code & this doc for leftovers
+
 
 Stretch goal:
-- Accept team number through the command line
 - Dynamically find the input file in the local directory to make the program platform agnostic
+- Use jxl to write season-persistent stats back to the spreadsheet so that lifetime numbers are tracked
+	- currently, there's no mechanism to separate season totals from lifetime totals. every 13 weeks,
+	  totals need to be written to the spreadsheet and new season figures should be started
+- Inter-divisional stats need to be implemented
+
+--------------------
+--------------------
+
+
+working notes below:
+
+[1] 		Lifetime W/L 
+[# teams - 1]	 Matchup W/L 
+[2]		Avg PF/PA (personal)
+[2]		High/Low PF (personal)
+[2]		High/Low PA (personal)
+[1]		H/A W/L (personal)
+[1]		H/A W/L (league)
+[1]		Kept drafted 
+[1]		Div record (personal)
+[1]		10 highest 
+[1]		10 lowest 
+[1]		Div/Div W/L 
+[2]		league total PF/PA 
+[2]		season PF/PA
+[2]		Avg margin of victory/defeat
+[2]		highest margin of victory/defeat
+[2]		lowest margin of victory/defeat
+[2]		league avg margin of victory/defeat
+[2]		league highest margein of victory/defeat
+[2]		league lowest margin of victory/defeat
+[1]		home team total points for season
+[1]		away team total points for season
+[1]		home team total points lifetime
+[1]		away team total points lifetime
+
+
+algorithm for massaging data:
+iterate through one person's full historical stats, adding them to a new 2d array
+calculate their stats, saving potential league relevant stats as you go
+when the calculations are done, store the resulting numbers in another new 2d array 
+go back to the extracted data, iterate through tht next person's full historical stats, adding them to the intermediate 2d array
+calculate their stats, saving / overwriting potential league relevant stats as you go
+when the calculations are done, store the resulting numbers in the next row of the final 2d array
+repeat for teamCountInt times
+
+
+ROW, COL
+[ROW][COL] FOR 2D ARRAY
--- Stat_Tracker/src/atm/stat/TrackerRobot/TrackerRobot.java
+++ Stat_Tracker/src/atm/stat/TrackerRobot/TrackerRobot.java
@@ -5,28 +5,70 @@ import jxl.*;
 
 public class TrackerRobot {
 	
-	final static int singleTeamDataSize = 8;
+	final static int SingleTeamDataSize = 8;
+	static int MassagedDataSizeBeforeTeamCountAdded = 30; //todo - likely to change
+	
+	//STATS STATS STATS 
+	//todo - inter-divisional stats not currently implemented
+    static int HomeTeamSeasonWins = 0;
+    static int AwayTeamSeasonWins = 0;
+    static int[] HighestSeasonScores = new int[10];
+    static int[] LowestSeasonScores = new int[10];
+    static int NumberOfDivisions;
+    static int SeasonTotalPointsScored = 0;
+    static int HomeTeamTotalPointsScoredSeason = 0;
+    static int AwayTeamTotalPointsScoredSeason = 0;
+    static int AverageMarginOfVictorySeason = 0;
+    static int HighestMarginOfVictorySeason = 0;
+    static int LowestMarginOfVictorySeason = 0;
+    
+    //NOTE: the implementation of these lifetime stats will change upon completion of a stretch goal
+    static int HomeTeamLifetimeWins = 0;
+    static int AwayTeamLifetimeWins = 0;
+    static int[] HighestLifetimeScores = new int[10];
+    static int[] LowestLifetimeScores = new int[10];
+    static int LeagueTotalPointsScored = 0;
+    static int HomeTeamTotalPointsScoredLifetime = 0;
+    static int AwayTeamTotalPointsScoredLifetime = 0;
+    static int AverageMarginOfVictoryLifetime = 0;
+    static int HighestMarginOfVictoryLifetime = 0;
+    static int LowestMarginOfVictoryLifetime = 0;
+    
+    //STATS STATS STATS
 
 	public static void main(String[] args) {
 		try 
 		{
 			//mac
-			 Workbook workbook = Workbook.getWorkbook(new File("/Users/alexmann/Developer/Fantasy_Stat_Tracking/inputFile_old.xls")); 	
+			 //Workbook workbook = Workbook.getWorkbook(new File("/Users/alexmann/Developer/Fantasy_Stat_Tracking/inputFile_old.xls")); 	
 			 
 			 //windows
-			 //Workbook workbook = Workbook.getWorkbook(new File("C:\\Users\\Alex\\Documents\\GitHub\\Fantasy_Stat_Tracking\\inputFile_old.xls")); 	
+			 Workbook workbook = Workbook.getWorkbook(new File("C:\\Users\\Alex\\Documents\\GitHub\\Fantasy_Stat_Tracking\\inputFile_old.xls")); 	
 
-			 
-			 //getting number of teams
-		     Sheet sheet = workbook.getSheet(0); 	     
+		     Sheet sheet = workbook.getSheet(0); 	  
+		     
+		     
 		     Cell a2 = sheet.getCell(0,1); 
 		     String teamCount = a2.getContents();
+		     int teamCountInt = Integer.parseInt(teamCount);
+		     int finalMassagedDataSize = teamCountInt - 1 + MassagedDataSizeBeforeTeamCountAdded;
+		     
+		     Cell b1 = sheet.getCell(1, 1);
+		     String numWeeks = b1.getContents();
+		     int numWeeksInt = Integer.parseInt(numWeeks);
+		     
+		     Cell a3 = sheet.getCell(0, 3);
+		     String numDivisions = a3.getContents();
+		     int numDivisionsInt = Integer.parseInt(numDivisions);
+		     NumberOfDivisions = numDivisionsInt;
 		     
 		     System.out.println("team count: " + teamCount);
+		     System.out.println("number of weeks: " + numWeeks);
+		     System.out.println("number of divisions: " + numDivisions);
 		     
-		     String[] data = extractData(sheet, teamCount);
-		     
-		     String[] fixedData = massageData(data);
+		        
+		     String[][] data = extractData(sheet, teamCountInt, numWeeksInt);
+		     String[][] fixedData = massageData(data, teamCountInt, finalMassagedDataSize, numWeeksInt);
 		     
 		} catch (Exception e)
 		{
@@ -34,35 +76,64 @@ public class TrackerRobot {
 		}
 	}
 
-	private static String[] extractData(Sheet sheet, String teamCount) {
-		int teamCountInt = Integer.parseInt(teamCount);
-		int dataSize = teamCountInt * singleTeamDataSize;
+	private static String[][] extractData(Sheet sheet, int teamCountInt, int numWeeksInt) {
 		
-		String[] data = new String[dataSize];
+		int dataSize = teamCountInt * SingleTeamDataSize;
+		
+		String[][] data = new String[numWeeksInt][dataSize];
 		int pos = 0;
-		int col = 1;
+		int col = 2;
 		int row = 1;
+		int week = 0;
 		
-		for (int x = 0; x < dataSize; x++)
+		for (int y = 0; y < numWeeksInt; y++)
 		{
-			Cell datum = sheet.getCell(col, row);
-			String datumString = datum.getContents();
-			data[pos] = datumString;
-			col++;
-			pos++;
+			for (int x = 0; x < dataSize; x++)
+			{
+				Cell datum = sheet.getCell(col, row);
+				String datumString = datum.getContents();
+				data[week][pos] = datumString;
+				col++;
+				pos++;
+			}
+			week++;
+			row++;
+			col = 2;
+			pos = 0;
 		}
-		
+	
 		return data;
 	}
 
-	private static String[] massageData(String[] data) {
-		String[] fixedData = new String[data.length]; //the length of fixedData might not need to be the same as data. keep that in mind
+	private static String[][] massageData(String[][] data, int teamCountInt, int finalMassagedDataSize, int numWeeksInt) {
 		
-		//loop X times, where X is number of teams
-		//store each individual datum in a container variable denoting what it is (name, points for, win/los, etc)
-		//calculate stats, store them in the new array 
-		//return
+		String[][] finalData = new String[teamCountInt][finalMassagedDataSize]; //this will be the result of this function
+		String[][] tempData = new String[numWeeksInt][SingleTeamDataSize]; //this is the temp container that will hold each person's full historical stats, one person at a time
 		
-		return fixedData;
+		tempData = extractOneTeam(data, numWeeksInt);
+		
+		//tempData now holds one team's lifetime data. build framework for massaging that data, and make sure that when you go back to pull more data you're pulling the next team (offset)
+		
+		return finalData;
+	}
+
+	private static String[][] extractOneTeam(String[][] data, int numWeeksInt) {
+		int offset = 0;
+		int row = 0;
+		int col = 0;
+		String[][] container = new String[numWeeksInt][SingleTeamDataSize];
+		
+		for (int y = 0; y < numWeeksInt; y++)
+		{
+			for (int x = 0; x < SingleTeamDataSize; x++)
+			{
+				container[row][col] = data[offset][x];
+				col++;
+			}
+			col = 0;
+			offset++;
+			row++;
+		}
+		return container;
 	}
 }
