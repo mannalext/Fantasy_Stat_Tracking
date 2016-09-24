06f366f1b9574f4625dc51ea8498cea02b072a31
southheat92 <southheat92@gmail.com>
2016-09-24 17:18:23 -0400

    continuing with TeamData
    added many more getters and setters for the members of TeamData

--- Stat_Tracker/src/atm/stat/TrackerRobot/TeamData.java
+++ Stat_Tracker/src/atm/stat/TrackerRobot/TeamData.java
@@ -2,92 +2,358 @@ package atm.stat.TrackerRobot;
 
 public class TeamData {
 	
-	private int pointsFor, totalPointsFor, pointsAgainst, totalPointsAgainst, totalGames;
-	private String name, division, opponent, location, result;
+	private static int pointsFor, totalPointsFor, avgPointsFor, highPointsFor, lowPointsFor, pointsAgainst,
+	totalPointsAgainst, avgPointsAgainst, highPointsAgainst, lowPointsAgainst, margin, totalMarginVictory, highMarginVictory, avgMarginVictory,
+	lowMarginVictory, totalMarginDefeat, highMarginDefeat, avgMarginDefeat, lowMarginDefeat, pointDifferential, totalGames, wins, losses,
+	homeWins, awayWins, divWins, nonDivWins, homeGames, awayGames, divGames, nonDivGames;
+	
+	private static String name, division, opponent, location, result;
 	/*make a 2 dimensional array here for matchup history
 	first dimension is team name (so X indices where X is number of teams)
 	second dimension is wins/losses against that particular team (2 indices)
 */
 
 	
-	public TeamData(String name, String division)
+	//constructor
+	public TeamData(String nameArg, String divisionArg)
 	{
-		this.name = name;
-		this.division = division;
+		name = nameArg;
+		division = divisionArg;
 	}
 	
-	
-	public int getPointsFor()
-	{
-		return this.pointsFor;
-	}
-	private void setPointsFor(int value)
-	{
-		this.pointsFor= value;
-	}
-	
-	public int getTotalPointsFor()
-	{
-		return this.totalPointsFor;
-	}
-	private void setTotalPointsFor()
-	{
-		this.totalPointsFor = this.totalPointsFor + this.pointsFor;
-	}
-	
-	public int getPointsAgainst()
-	{
-		return this.pointsAgainst;
-	}
-	private void setPointsAgainst(int value)
-	{
-		this.pointsAgainst = value;
-	}
-	
-	public int getTotalPointsAgainst()
-	{
-		return this.totalPointsAgainst;
-	}
-	private void setTotalPointsAgainst()
-	{
-		this.totalPointsAgainst = this.totalPointsAgainst + this.pointsAgainst;
-	}
-	
-	public String getResult()
-	{
-		return this.result;
-	}
-	private void setResult()
-	{
-		if (getPointsFor() > getPointsAgainst())
-		{
-			this.result = "w";
-		} else if (getPointsFor() < getPointsAgainst())
-		{
-			this.result = "l";
-		} else if (getPointsFor() == getPointsAgainst())
-		{
-			this.result = "t";
-		}
-	}
 	
 	public String getOpponent()
 	{
-		return this.opponent;
+		return opponent;
 	}
 	private void setOpponent(String value)
 	{
 		opponent = value;
 	}
 	
+	public String getLocation()
+	{
+		return location;
+	}
+	private void setLocation(String value)
+	{
+		location = value;
+	}
+	
+	public int getPointsFor()
+	{
+		return pointsFor;
+	}
+	private void setPointsFor(int value)
+	{
+		pointsFor= value;
+	}
+	
+	public int getTotalPointsFor()
+	{
+		return totalPointsFor;
+	}
+	private void setTotalPointsFor()
+	{
+		totalPointsFor = totalPointsFor + pointsFor;
+	}
+	
+	public int getAvgPointsFor()
+	{
+		return avgPointsFor;
+	}
+	private void setAvgPointsFor()
+	{
+		avgPointsFor = totalPointsFor / totalGames;
+	}
+	
+	public int getHighPointsFor()
+	{
+		return highPointsFor;
+	}
+	private void setHighPointsFor()
+	{
+		if (pointsFor > highPointsFor)
+		{
+			highPointsFor = pointsFor;
+		}
+	}
+	
+	public int getLowPointsFor()
+	{
+		return lowPointsFor;
+	}
+	private void setLowPointsFor()
+	{
+		if (pointsFor < lowPointsFor)
+		{
+			lowPointsFor = pointsFor;
+		}
+	}
+	
+	public int getPointsAgainst()
+	{
+		return pointsAgainst;
+	}
+	private void setPointsAgainst(int value)
+	{
+		pointsAgainst = value;
+	}
+	
+	public int getTotalPointsAgainst()
+	{
+		return totalPointsAgainst;
+	}
+	private void setTotalPointsAgainst()
+	{
+		totalPointsAgainst = totalPointsAgainst + pointsAgainst;
+	}
+	
+	public int getAvgPointsAgainst()
+	{
+		return avgPointsAgainst;
+	}
+	private void setAvgPointsAgainst()
+	{
+		avgPointsAgainst = totalPointsAgainst / totalGames;
+	}
+	
+	public int getHighPointsAgainst()
+	{
+		return highPointsAgainst;
+	}
+	private void setHighPointsAgainst()
+	{
+		if (pointsAgainst > highPointsAgainst)
+		{
+			highPointsAgainst = pointsAgainst;
+		}
+	}
+	
+	public int getLowPointsAgainst()
+	{
+		return lowPointsAgainst;
+	}
+	private void setLowPointsAgainst()
+	{
+		if (pointsAgainst < lowPointsAgainst)
+		{
+			lowPointsAgainst = pointsAgainst;
+		}
+	}
+	
+	//when deciding whether to get margin victory or defeat, do a check on the return value of getMargin to see if it's positive or negative
+	public int getMargin()
+	{
+		return margin;
+	}
+	private void setMargin()
+	{
+		margin = pointsFor - pointsAgainst;
+	}
+	
+	public int getTotalMarginVictory()
+	
+	private void setTotalMarginVictory()
+	
+	public int getHighMarginVictory()
+	{
+		return highMarginVictory;
+	}
+	private void setHighMarginVictory()
+	{
+		if (margin > highMarginVictory)
+		{
+			highMarginVictory = margin;
+		}
+	}
+	
+	public int getLowMarginVictory()
+	
+	private void setLowMarginVictory()
+	
+	
+	public int getAvgMarginVictory()
+	
+	private void setAvgMarginVictory()
+	
+	
+	public int getTotalMarginDefeat()
+	
+	private void setTotalMarginDefeat()
+	
+	
+	public int getHighMarginDefeat()
+	
+	private void setHighMarginDefeat()
+	
+	
+	public int getLowMarginDefeat()
+	
+	private void setLowMarginDefeat()
+	
+	
+	public int getAvgMarginDefeat()
+	
+	private void setAvgMarginDefat()
+	
+	
+	public int getPointDifferential()
+	
+	private void setPointDifferential()
+	
+	
+	public String getResult()
+	{
+		return result;
+	}
+	private void setResult()
+	{
+		if (pointsFor > pointsAgainst)
+		{
+			result = "w";
+		} else if (pointsFor < pointsAgainst)
+		{
+			result = "l";
+		} else if (pointsFor == pointsAgainst)
+		{
+			result = "t";
+		}
+	}
+	
+	public int getWins()
+	{
+		return wins;
+	}
+	private void setWins()
+	{
+		wins++;
+	}
+	
+	public int getLosses()
+	{
+		return losses;
+	}
+	private void setLosses()
+	{
+		losses++;
+	}
+	
 	public int getTotalGames()
 	{
-		return this.totalGames;
+		return totalGames;
 	}
-	private void setTotalGames(int value)
+	private void setTotalGames()
 	{
-		this.totalGames = value;
+		totalGames++;
 	}
+	
+	public int getHomeGames()
+	{
+		return homeGames;
+	}
+	private void setHomeGames()
+	{
+		homeGames++;
+	}
+	
+	public int getHomeWins()
+	{
+		return homeWins;
+	}
+	private void setHomeWins()
+	{
+		homeWins++;
+	}
+	
+	public int getAwayGames()
+	{
+		return awayGames;
+	}
+	private void setAwayGames()
+	{
+		awayGames++;
+	}
+	
+	public int getAwayWins()
+	{
+		return awayWins;
+	}
+	private void setAwayWins()
+	{
+		awayWins++;
+	}
+	
+	public int getDivGames()
+	{
+		return divGames;
+	}
+	private void setDivGames()
+	{
+		divGames++;
+	}
+	
+	public int getDivWins()
+	{
+		return divWins;
+	}
+	private void setDivWins()
+	{
+		divWins++;
+	}
+	
+	public int getNonDivGames()
+	{
+		return nonDivGames;
+	}
+	private void setNonDivGames()
+	{
+		nonDivGames++;
+	}
+	
+	public int getNonDivWins()
+	{
+		return nonDivWins;
+	}
+	private void setNonDivWins()
+	{
+		nonDivWins++;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	//calculation functions
+	public double calculateOverallWinPct()
+	{
+		return (getWins() / getTotalGames());
+	}
+	
+	public double calculateHomeWinPct()
+	{
+		return (getHomeWins() / getHomeGames());
+	}
+	
+	public double calculateAwayWinPct()
+	{
+		return (getAwayWins() / getAwayGames());
+	}
+	
+	public double calculateDivWinPct()
+	{
+		return (getDivWins() / getDivGames());
+	}
+	
+	public double calculateNonDivWinPct()
+	{
+		return (getNonDivWins() / getNonDivGames());
+	}
+
+	
 
 	
 }
